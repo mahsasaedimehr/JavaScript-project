7a95c4f0c692eae7bb7c84dbad8c84b7041bb6c4
mahsasaedimehr <mahsa.saedimehr@gmail.com>
2023-08-11 22:44:05 +0330

    add personal list

---
+++ indwx.js
@@ -0,0 +1,30 @@
+const personList = [
+    {
+        name:"mahsa",
+        age:"22",
+        id:"mahsa.sm",
+        degree:"computer",
+    },
+
+    {
+        name:"shakiba",
+        age:"23",
+        id:"sh.lkr",
+        degree:"computer",
+    },
+
+    {
+        name:"farhad",
+        age:"24",
+        id:"frhd_lkr",
+        degree:"computer",
+    },
+
+    {
+        name:"maryam",
+        age:"32",
+        id:"maryam_salehi",
+        degree:"dentis",
+    }
+
+];
